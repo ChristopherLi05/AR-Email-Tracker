84b79cebc4590090a6c5a054b55df3d1162eb6e0
a <>
2024-08-19 16:56:53 -0400

    extract code to main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import data_parser
+
+
+def main():
+    manager = data_parser.TrackerManager()
+    manager.load_tracker_csv("data/mps_tracker_export.csv")
+    data = data_parser.extract_emails("data/email_exports/chris_email_export.pst")
+
+    unknown_emails = manager.compile_emails(data)
+
+    print(unknown_emails)
+    print(len(unknown_emails))
+
+    # unknown = manager.compile_emails(data)
+    # emails = set()
+    #
+    # for i in unknown:
+    #     emails.update(i.emails)
+    #
+    # print(emails)
+
+    # data1 = compile_emails(data)
+
+    # for i, j in data1.items():
+    #     print(i, len(j))
+
+    # print(extract_tracker("data/mps_tracker_export.csv"))
+    # data = extract_tracker("data/mps_tracker_export.csv")
+    # for i in range(len(data)):
+    #     for j in range(i + 1, len(data)):
+    #         if data[i].is_same_person(data[j]):
+    #             print(data[i], data[j])
+    #             input()
+
+
+if __name__ == "__main__":
+    main()
